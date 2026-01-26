2c1501cc8210b8105166b1d5756494ac7224f5b8
Khurram Qamar <khurramqamar@Khurrams-MacBook-Air.local>
2026-01-26 22:20:59 +0000

    ESLint error 1

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Mail } from 'lucide-react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card';
+import { Card, CardContent,CardHeader, CardTitle } from '../components/ui/card';
 import { Button } from '../components/ui/button';
 
 const ContactPage = () => {
