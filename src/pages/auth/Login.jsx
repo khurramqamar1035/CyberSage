a8cb2a402da6e179d8405fce2d9a65ebc04df958
Khurram Qamar <khurramqamar@Khurrams-MacBook-Air.local>
2026-04-01 03:26:10 +0100

    signup and verification of email added 2

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
 
     try {
 
-      const response = await fetch("http://localhost:5001/api/auth/login", {
+      const response = await fetch(`${BACKEND_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
